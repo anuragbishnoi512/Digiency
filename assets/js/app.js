f733efcbf284063998f394dd683a692228ac23c2
anurag-512 <anuragsharan51@gmail.com>
2024-04-17 10:16:09 +0530

    section-6 done

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,7 @@ $('.slider').slick({
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-                    autoplay: true,
+                autoplay: true,
                 autoplaySpeed: 2000,
             }
         },
@@ -41,3 +41,22 @@ $('.slider').slick({
         }
     ]
 });
+
+
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}   
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
