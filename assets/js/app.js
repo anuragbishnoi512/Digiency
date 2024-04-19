8701f3bb73c707f25a4f381af1b37b14653e06b5
anurag-512 <anuragsharan51@gmail.com>
2024-04-19 11:39:29 +0530

    footer section done responsive started

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
+let menuicon = document.querySelector(".nav-menu");
+let menulist = document.querySelector(".nav-items");
+
+menuicon.addEventListener("click", function () {
+    menulist.classList.toggle("show");
+})
+
+
+
+
 
 $('.slider').slick({
     dots: false,
